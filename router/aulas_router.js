54820e83eb78ba81776b9d9f24cb2e0a25c237c2
TrilhaX <joaotrilha01@gmail.com>
2025-04-30 10:50:49 -0300

    Add MVC to code

--- router/aulas_router.js
+++ router/aulas_router.js
@@ -1,27 +1,18 @@
 const express = require('express')
 const {writeFile, readFile} = require('../model/aulas_model')
+const { getAllClasses, getSpecificClass, createClass, updateClass, deleteClass } = require('../control/control_aulas')
 const router_aulas = express.Router()
 
 
 //incluir as rotas que fazem parte deste roteador
-router_aulas.get('', (req, res) => {
-    res.status(200).send(bancodeDados)
-})
+router_aulas.get('', getAllClasses)
 
-router_aulas.get('/:id', (req, res) => {
-    readFile('bancoDeDados.json', 'utf-8', 'get', req, res)
-})
+router_aulas.get('/:id', getSpecificClass)
 
-router_aulas.post('', (req, res) => {
-    readFile('bancoDeDados.json', 'utf-8', 'post', req, res)
-})
+router_aulas.post('', createClass)
 
-router_aulas.put('/:id', (req, res) => {
-    readFile('bancoDeDados.json', 'utf-8', 'put', req, res)
-});
+router_aulas.put('/:id', updateClass)
 
-router_aulas.delete('/:id', (req, res) => {
-    readFile('bancoDeDados.json', 'utf-8', 'delete', req, res)
-});
+router_aulas.delete('/:id', deleteClass);
 
 module.exports = router_aulas
